a8ab047f87c7a151f5beb0a5a5d592be8711f5ea
dalishadisaputra <hadhieringgo@gmail.com>
2022-06-08 19:27:41 +0700

    Initial Commit

---
+++ Dasar/script.js
@@ -0,0 +1,26 @@
+// coding awal hello world
+// document.writeln("Hello World")
+
+// tipe data number
+// document.writeln(100); // tipe data number bulat
+// document.writeln("</br>");
+// document.writeln(100.123); //tipe data number desimal
+// document.writeln("</br>");
+// document.writeln(0b1111010); //tipe data number binary
+
+// tipe data boolean
+// document.writeln(true);
+// document.writeln("</br>");
+// document.writeln(false);
+
+// kode menambah string
+// document.writeln("Nama : " + "Hadi" + " Saputra");
+// document.writeln("</br>");
+// document.writeln("Channel : " + "Programmer Zaman Now");
+
+// Escape Sequence
+document.writeln("<textarea cols='100' rows='10'>")
+document.writeln("Dalis \nHadi \nSaputra");
+document.writeln("\\Home \\Hadi")
+document.writeln("\")
+document.writeln("<textarea>")
